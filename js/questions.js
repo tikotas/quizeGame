bbd2148f7f0e894752683a8eac1fe62e3e479678
Arsenich <tikotas@mail.ru>
2021-10-20 02:40:43 +0400

    Quiz was upgraded

---
+++ js/questions.js
@@ -0,0 +1,121 @@
+
+const questions = [
+
+{
+  quest: "Ո՞րն է Գերմանիայի մայրաքաղաքը։",
+  variant1: "Լոնդոն",
+  variant2: "Փարիզ",
+  variant3: "Բեռլին",
+  variant4: "Համբուրգ",
+  rightAnswer: "Բեռլին"
+},
+
+{
+  quest: "Նշվածներից ո՞րը քիմիական տարր չէ։",
+  variant1: "Էլիբրիում",
+  variant2: "Ալյումինիում",
+  variant3: "Գերմանիում",
+  variant4: "Կուպրում",
+  rightAnswer: "Էլիբրիում"
+},
+
+{
+  quest: "Ե՞րբ է տեղի ունեցել Ավարայրի ճակատամարտը։",
+  variant1: "403թ․",
+  variant2: "405թ․",
+  variant3: "451թ․",
+  variant4: "541թ․",
+  rightAnswer: "451թ․"
+},
+
+{
+  quest: "Ո՞րն է աշխարհի ամենաերկար գետը։",
+  variant1: "Ամազոն",
+  variant2: "Միսիսիպի",
+  variant3: "Գանգես",
+  variant4: "Նեղոս",
+  rightAnswer: "Նեղոս"
+},
+
+{
+  quest: "Քանի՞ օվկիանոս կա երկրի վրա։",
+  variant1: "4",
+  variant2: "3",
+  variant3: "5",
+  variant4: "7",
+  rightAnswer: "5"
+},
+
+{
+  quest: "Թվով ո՞րերորդ մոլորակն է հանդիսանում Յուպիտերը։",
+  variant1: "3",
+  variant2: "5",
+  variant3: "6",
+  variant4: "4",
+  rightAnswer: "5"
+},
+
+{
+  quest: "Ե՞րբ է տեղի ունեցել Գավգամելայի ճակատամարտը։",
+  variant1: "մ․թ․ա․ 331թ․",
+  variant2: "մ․թ․ա․ 323թ․",
+  variant3: "մ․թ․ա․ 451թ․",
+  variant4: "մ․թ․ա․ 301թ․",
+  rightAnswer: "մ․թ․ա․ 331թ․"
+},
+
+{
+  quest: "Որքա՞ն կկազմի 11 թվի քառակուսին",
+  variant1: "111",
+  variant2: "121",
+  variant3: "221",
+  variant4: "112",
+  rightAnswer: "121"
+},
+
+{
+  quest: "Քանի՞ մետր է 1 կմ։",
+  variant1: "100մ",
+  variant2: "1000մ",
+  variant3: "10000մ",
+  variant4: "10մ",
+  rightAnswer: "1000մ"
+},
+
+{
+  quest: "Ես իմ անուշ Հայաստանի ․․․ բառն եմ սիրում։",
+  variant1: "նրբահամ",
+  variant2: "արևահամ",
+  variant3: "նրբանկատ",
+  variant4: "արևահարված",
+  rightAnswer: "արևահամ"
+},
+
+{
+  quest: 'Նշվածներից ո՞րն է "սնգույր" բառի հոմանիշը։',
+  variant1: "Թախիծ",
+  variant2: "Դատարկ",
+  variant3: "Կախիչ",
+  variant4: "Շպար",
+  rightAnswer: "Շպար"
+},
+
+{
+  quest: 'Ու՞մ են պատկանում " Եվ դու՞ Բրուտոս " խոսքերը։',
+  variant1: "Ա. Մակեդոնացի",
+  variant2: "Մ. Կրասոս",
+  variant3: "Հ. Կեսար",
+  variant4: "Կալիգուլա",
+  rightAnswer: "Հ. Կեսար"
+},
+
+{
+  quest: 'Մուսուլման ո՞ր զորավարն է գրավել Երուսաղեմը։',
+  variant1: "Չինգիզ Խան",
+  variant2: "Սալլահ Հադին",
+  variant3: "Իբրագիմ Փաշա",
+  variant4: "Սուլթան Սուլեյման",
+  rightAnswer: "Սալլահ Հադին"
+},
+
+];
